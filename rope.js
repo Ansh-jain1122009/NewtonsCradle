507bad8bec2f6de63b2e41e0a12f8dc170c6f73f
Ansh-jain1122009 <86413791+Ansh-jain1122009@users.noreply.github.com>
2021-10-01 21:02:48 +0530

    Add files via upload

--- rope.js
+++ rope.js
@@ -1,31 +1,25 @@
 class rope{
-	constructor(body1,body2,pointA,pointB)
+	constructor(body1,body2, pointA, pointB)
 	{
 		this.pointA=pointA
 		this.pointB=pointB
-
+	
 		var options={
 			bodyA:body1,
 			bodyB:body2,
-			pointB:{x:this.pointA,y:this.pointB}
+			pointB:{x:this.pointA, y:this.pointB}
 		}
-
-	//create rope constraint here
-		this.body= Constraint.create(options)
-        World.add(world, this.body);
-
+		this.rope=Constraint.create(options)
+		World.add(world,this.rope)
 	}
 
+	display()
+	{
+		var pointA=this.rope.bodyA.position;
+		var pointB=this.rope.bodyB.position;
 
-    //create display() here 
-	display() {
-		let pos = this.body.position;
-	
-		push();
-		rectMode(CENTER);
-		stroke(255);
-		fill(127);
-		rect(pos.x, pos.y, this.w, this.h);
-		pop();
-	  }
+		strokeWeight(2);
+		line(pointA.x,pointA.y,pointB.x+this.pointA,pointB.y+this.pointB);
+	}
+
 }
--- sketch.js
+++ sketch.js
@@ -5,10 +5,11 @@ const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
 var roof;
-//Create multiple bobs, mulitple ropes varibale here
-var bob1,bob2,bob3,bob4,bob5;
-var rope1,rope2,rope3,rope4,rope5;
-var con,con2,con3,con4,con5;
+var bob1,bob2,bob3, bob4,bob5;
+var rope1,rope2,rope3, rope4,rope5;
+var world;
+
+
 function setup() {
 	createCanvas(800, 600);
 	rectMode(CENTER);
@@ -16,72 +17,42 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	var bob_options ={
-		restitution: 0.95,
-	  }
-
-
 	var roof_options={
 		isStatic:true			
 	}
 
-	rope1= new rope(bob1,roof,-80,0);
-	rope2= new rope(bob2,roof,-80,0);
-	rope3= new rope(bob3,roof,-80,0);
-	rope4= new rope(bob4,roof,-80,0);
-	rope5= new rope(bob5,roof,-80,0);
-
-	bob1 = Bodies.circle(300,200,20,bob_options);
-	World.add(world,bob1);
-	bob2 = Bodies.circle(350,200,20,bob_options);
-	World.add(world,bob2);
-	bob3 = Bodies.circle(400,200,20,bob_options);
-	World.add(world,bob3);
-	bob4 = Bodies.circle(450,200,20,bob_options);
-	World.add(world,bob4);
-	bob5 = Bodies.circle(500,200,20,bob_options);
-	World.add(world,bob5);
-
-
 	roof = Bodies.rectangle(400,100,230,20,roof_options);
     World.add(world,roof);
 
-	con = Matter.Constraint.create({
-		pointA:{x:300,y:100},
-		bodyB:ball,
-		length:100,
-		stiffness:0.1 
-	   })
-	   World.add(world,con);
-	con2 = Matter.Constraint.create({
-		pointA:{x:350,y:100},
-		bodyB:ball,
-		length:100,
-		stiffness:0.1 
-	   })
-	   World.add(world,con2);
-	con3 = Matter.Constraint.create({
-		pointA:{x:400,y:100},
-		bodyB:ball,
-		length:100,
-		stiffness:0.1 
-	   })
-	   World.add(world,con3);
-	con4 = Matter.Constraint.create({
-		pointA:{x:450,y:100},
-		bodyB:ball,
-		length:100,
-		stiffness:0.1 
-	   })
-	   World.add(world,con4);
-    con5 = Matter.Constraint.create({
-		pointA:{x:500,y:100},
-		bodyB:ball,
-		length:100,
-		stiffness:0.1 
-	   })
-	   World.add(world,con5);
+	var bob_options={
+		isStatic:false,
+		restitution:1,
+		friction:0,
+		density:0.8	
+	}
 
+	bob1 =Bodies.circle(320,380,20,bob_options);
+    World.add(world,bob1);
+
+	bob2 = Bodies.circle(360,380,20,bob_options);
+    World.add(world,bob2);
+
+	bob3 = Bodies.circle(400,380,20,bob_options);
+    World.add(world,bob3);
+
+	bob4 = Bodies.circle(440,380,20,bob_options);
+    World.add(world,bob4);
+
+	bob5 = Bodies.circle(480,380,20,bob_options);
+    World.add(world,bob5);
+	
+	
+	rope1=new rope(bob1,roof,-80, 0)
+	rope2=new rope(bob2,roof,-40, 0)
+	rope3=new rope(bob3,roof,0, 0)
+	rope4=new rope(bob4,roof,40, 0)
+	rope5=new rope(bob5,roof,80, 0)
+	
 	Engine.run(engine);
 	
   
@@ -93,24 +64,23 @@ function draw() {
 
   rect(roof.position.x,roof.position.y,230,20);
 
-  //call display() to show ropes here
   rope1.display();
   rope2.display();
   rope3.display();
   rope4.display();
-  rope5.display();  
-
-  //create ellipse shape for multiple bobs here
-  ellipse(bob1.position.x,bob1.position.y,20);
-  ellipse(bob2.position.x,bob2.position.y,20);
-  ellipse(bob3.position.x,bob3.position.y,20);
-  ellipse(bob4.position.x,bob4.position.y,20);
-  ellipse(bob5.position.x,bob5.position.y,20);
+  rope5.display();
+  
+  ellipse(bob1.position.x,bob1.position.y,40,40);
+  ellipse(bob2.position.x,bob2.position.y,40,40);
+  ellipse(bob3.position.x,bob3.position.y,40,40);
+  ellipse(bob4.position.x,bob4.position.y,40,40);
+  ellipse(bob5.position.x,bob5.position.y,40,40);
  
 }
 
-//Write keyPressed function and apply force on pressing up_arrow key on the first bob.
-function keyPressed() { 
-	if (keyCode === UP_ARROW) { 
-		Matter.Body.applyForce(ball,ball.position,{x:-85,y:85}); 
-	} }
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(bob1,bob1.position,{x:-50,y:-45});
+	}
+}
+
